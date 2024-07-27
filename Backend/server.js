188be51f4ff2a0fd21aaa91f00e8fff4c5058679
Natannan Zeleke <natannanz16@ethioware.org>
2024-07-27 13:57:06 +0300

    feat: Add backend starting code

---
+++ Backend/server.js
@@ -0,0 +1,17 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const dotenv = require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`)
+});
