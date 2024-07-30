2fbaad425e2f69f8e676048cdb3798a5373730d4
Nasifay-Chala <nasifayc11@ethioware.org>
2024-07-30 10:59:51 +0300

    feat: new APIs added

--- app/lib/core/api.dart
+++ app/lib/core/api.dart
@@ -6,5 +6,14 @@ class ApiUrl {
       "$baseUrl/api/institutions/registerInstitution";
   static const String professionalsRegistrationUrl =
       "$baseUrl/api/professions/registerProfession";
+  static const String adminRegistrationUrl =
+      "$baseUrl/api/admins/registerAdmin";
+  static const String clientLoginUrl = "$baseUrl/api/clients/loginClient";
+  static const String institutionLoginUrl =
+      "$baseUrl/api/institutions/loginInstitution";
+  static const String professionalLoginUrl =
+      "$baseUrl/api/professions/loginProfessional";
+  static const String adminLoginUrl = "$baseUrl/api/admins/loginAdmin";
   static const String logoutUrl = "$baseUrl/api/logout";
+  static const String tokenVerificationUrl = "$baseUrl/api/verify-token";
 }
