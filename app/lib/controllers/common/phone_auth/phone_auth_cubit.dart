e205c65d9d1f4b9f5c79f35346eed8613c2575a9
Nasifay-Chala <nasifayc11@ethioware.org>
2024-09-06 17:07:19 +0300

    fix: error fixed on the professionals registration form

--- app/lib/controllers/common/phone_auth/phone_auth_cubit.dart
+++ app/lib/controllers/common/phone_auth/phone_auth_cubit.dart
@@ -1,54 +1,51 @@
-import 'dart:developer' as developer;
-import 'dart:math';
+// import 'dart:developer' as developer;
+// import 'dart:math';
 
-import 'package:app/controllers/common/phone_auth/phone_auth_state.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:twilio_flutter/twilio_flutter.dart';
+// import 'package:app/controllers/common/phone_auth/phone_auth_state.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:twilio_flutter/twilio_flutter.dart';
 
-class PhoneAuthCubit extends Cubit<PhoneAuthState> {
-  final String accountSID = 'ACb39f6a7fa5374317c08c0c2fec1afd3b';
-  final String authToken = '31250462d5673bd5c041f4d388b0f6bb';
-  final String tiwiloNumber = '+251900624702';
-  late TwilioFlutter twilioFlutter;
+// class PhoneAuthCubit extends Cubit<PhoneAuthState> {
+  
 
-  PhoneAuthCubit() : super(PhoneAuthInitial());
+//   PhoneAuthCubit() : super(PhoneAuthInitial());
 
-  void sendSMS(String phoneNumber) async {
-    twilioFlutter = TwilioFlutter(
-        accountSid: accountSID,
-        authToken: authToken,
-        twilioNumber: tiwiloNumber);
+//   void sendSMS(String phoneNumber) async {
+//     twilioFlutter = TwilioFlutter(
+//         accountSid: accountSID,
+//         authToken: authToken,
+//         twilioNumber: tiwiloNumber);
 
-    var rnd = Random();
+//     var rnd = Random();
 
-    var digits = rnd.nextInt(900000) + 100000;
-    developer.log(digits.toString());
+//     var digits = rnd.nextInt(900000) + 100000;
+//     developer.log(digits.toString());
 
-    emit(PhoneAuthLoading());
-    try {
-      final result = await twilioFlutter.sendSMS(
-          toNumber: phoneNumber,
-          messageBody:
-              'Hello This is 6 digit otp code to verify phone $digits');
-      developer.log(result.toString());
-      if (result.responseCode != 200) {
-        emit(PhoneAuthError(
-            errorMessage: 'Failed to send SMS: ${result.toString()}'));
-      } else {
-        emit(PhoneAuthVerifyCode(
-            verificationCode: digits.toString(), phoneNumber: phoneNumber));
-      }
-    } catch (e) {
-      emit(PhoneAuthError(errorMessage: e.toString()));
-    }
-  }
+//     emit(PhoneAuthLoading());
+//     try {
+//       final result = await twilioFlutter.sendSMS(
+//           toNumber: phoneNumber,
+//           messageBody:
+//               'Hello This is 6 digit otp code to verify phone $digits');
+//       developer.log(result.toString());
+//       if (result.responseCode != 200) {
+//         emit(PhoneAuthError(
+//             errorMessage: 'Failed to send SMS: ${result.toString()}'));
+//       } else {
+//         emit(PhoneAuthVerifyCode(
+//             verificationCode: digits.toString(), phoneNumber: phoneNumber));
+//       }
+//     } catch (e) {
+//       emit(PhoneAuthError(errorMessage: e.toString()));
+//     }
+//   }
 
-  void verifyOTP(String sentCode, String userCode) {
-    emit(PhoneAuthLoading());
-    if (sentCode == userCode) {
-      emit(PhoneAuthVerified(success: 'Successfully Verfied'));
-    } else {
-      emit(PhoneAuthError(errorMessage: 'Authentication Faild'));
-    }
-  }
-}
+//   void verifyOTP(String sentCode, String userCode) {
+//     emit(PhoneAuthLoading());
+//     if (sentCode == userCode) {
+//       emit(PhoneAuthVerified(success: 'Successfully Verfied'));
+//     } else {
+//       emit(PhoneAuthError(errorMessage: 'Authentication Faild'));
+//     }
+//   }
+// }
--- app/lib/controllers/common/phone_auth/phone_auth_state.dart
+++ app/lib/controllers/common/phone_auth/phone_auth_state.dart
@@ -1,25 +1,25 @@
-abstract class PhoneAuthState {}
+// abstract class PhoneAuthState {}
 
-final class PhoneAuthInitial extends PhoneAuthState {}
+// final class PhoneAuthInitial extends PhoneAuthState {}
 
-final class PhoneAuthLoading extends PhoneAuthState {}
+// final class PhoneAuthLoading extends PhoneAuthState {}
 
-final class PhoneAuthVerifyCode extends PhoneAuthState {
-  final String verificationCode;
-  final String phoneNumber;
+// final class PhoneAuthVerifyCode extends PhoneAuthState {
+//   final String verificationCode;
+//   final String phoneNumber;
 
-  PhoneAuthVerifyCode(
-      {required this.verificationCode, required this.phoneNumber});
-}
+//   PhoneAuthVerifyCode(
+//       {required this.verificationCode, required this.phoneNumber});
+// }
 
-final class PhoneAuthVerified extends PhoneAuthState {
-  final String success;
+// final class PhoneAuthVerified extends PhoneAuthState {
+//   final String success;
 
-  PhoneAuthVerified({required this.success});
-}
+//   PhoneAuthVerified({required this.success});
+// }
 
-final class PhoneAuthError extends PhoneAuthState {
-  final String errorMessage;
+// final class PhoneAuthError extends PhoneAuthState {
+//   final String errorMessage;
 
-  PhoneAuthError({required this.errorMessage});
-}
+//   PhoneAuthError({required this.errorMessage});
+// }
