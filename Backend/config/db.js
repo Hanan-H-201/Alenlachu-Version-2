a315ef4fa68f679cbe666fcb1704701b3b0d1467
Hanan-H-201 <hananhusein201@ethioware.org>
2025-05-03 22:35:55 +0300

    Fix MongoDB connection warnings and update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,10 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import mongoose from 'mongoose';
 
 const mongoUri = process.env.MONGO_URL;
 
-mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(()=>console.log("MongoDB Connected"))
-.catch((err)=>console.log(err));
-
+mongoose.connect(mongoUri)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 export default mongoose;
